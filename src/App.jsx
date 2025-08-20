3d83c873a54b5443d0c29aa4d735a985980ebf77
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-20 11:35:53 +0400

    💄 setup front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { LandingPage } from './screens/LandingPage'
+import Dons from './screens/Dons'
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dons" element={<Dons />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
