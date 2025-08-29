89c9661183c32698aa47f8f3c99617c3349256ac
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-29 14:57:11 +0400

    ✨ Add navbar

--- src/partials/Boilerplate.jsx
+++ src/partials/Boilerplate.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Navbar from "./Navbar";
 
 const Boilerplate = () => {
 return (
     <div className="d-flex flex-column vh-100">
+        <Navbar />
         <main className="container mt-5">
             <Outlet />
         </main>
