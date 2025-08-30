54cd5a6e1f0a03764456d860c9d9e7e92d19a053
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-30 16:43:19 +0400

    ✨ Add footer

--- src/partials/Boilerplate.jsx
+++ src/partials/Boilerplate.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
+import { Footer } from "./Footer";
 
 const Boilerplate = () => {
 return (
@@ -8,6 +9,7 @@ return (
         <main className="container mt-5">
             <Outlet />
         </main>
+        <Footer/>
     </div>
 )
 
