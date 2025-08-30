f59760dd7ce33876d59f9af70195d2cb2b8b2c50
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-08-30 18:37:24 +0400

    💄 New : add style

--- src/screens/New.jsx
+++ src/screens/New.jsx
@@ -8,7 +8,9 @@ const New = () => {
         e.preventDefault();
         const don = {
             title: e.target[0].value,
-            location: e.target[1].value
+            location: e.target[1].value,
+            image: e.target[2].value,
+            description: e.target[3].value
         };
         await axios
                 .post("/dons/new", don)
@@ -21,21 +23,64 @@ const New = () => {
                 });
     }
     return (
-        <>
-        <h1>Donnez !</h1>
-        <form onSubmit={onFormSubmit}>
-            <div>
-                <label htmlFor="title">titre</label>
-                <input type="text" id="title" name="title" />
+    <>
+      <div className="row">
+        <h1 className="text-center">Donnez vos objets</h1>
+        <div className="col-6 offset-3">
+          <form onSubmit={onFormSubmit}>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="title">
+                Je donne
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="title"
+                name="title"
+                placeholder="par exemple, un canapé"
+              />
             </div>
-            <div>
-                <label htmlFor="location">Localisation</label>
-                <textarea id="location" name="location"></textarea>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="location">
+                où ?
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="location"
+                name="location"
+                placeholder="Par exemple, à Saint-Denis"
+              />
             </div>
-            <button>Submit</button>
-            <a href="/dons">Retourner à la liste des dons</a>
-        </form>
-        </>
-    )
+            <div className="mb-3">
+              <label className="form-label" htmlFor="image">
+                Photo de l'objet
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="image"
+                name="image"
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="description">
+                Description
+              </label>
+              <textarea
+                className="form-control"
+                type="text"
+                id="description"
+                name="description"
+              />
+            </div>
+            <div className="mb-3">
+              <button className="btn btn-success">Donnez!</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 export default New
